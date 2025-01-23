4557d2965bd12171398838055482b58726ef7f97
ChamodiDH <chamodi3797@gmail.com>
2025-01-23 15:45:15 +0530

    Initialize the spring boot project with dependencies.

---
+++ ResucueSync/ResucueSync/src/main/java/com/rescureSync/ResucueSync/ResucueSyncApplication.java
@@ -0,0 +1,13 @@
+package com.rescureSync.ResucueSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResucueSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResucueSyncApplication.class, args);
+	}
+
+}
---
+++ ResucueSync/ResucueSync/src/test/java/com/rescureSync/ResucueSync/ResucueSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rescureSync.ResucueSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResucueSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
